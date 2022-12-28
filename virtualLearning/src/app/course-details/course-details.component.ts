54fb6db085840e04a21605a985f7f7cf021a79ac
NagaveniGopalNaik <nagaveni.naik@robosoftin.com>
2022-12-28 17:45:21 +0530

    Basic UI part done

--- virtualLearning/src/app/course-details/course-details.component.ts
+++ virtualLearning/src/app/course-details/course-details.component.ts
@@ -9,6 +9,7 @@ export class CourseDetailsComponent implements OnInit {
 display_name=true;
 course_details_active = true;
 display_sub_chapter = false;
+mobile_video_display=false;
   constructor() { }
 
   ngOnInit(): void {
@@ -26,6 +27,13 @@ this.display_name = false;
   }
   displaySubChapter(){
     this.display_sub_chapter = !this.display_sub_chapter;
+  }
+
+  displayVideo(){
+    this.mobile_video_display = true;
+  }
+  closeVideo(){
+    this.mobile_video_display = false;
   }
 
 }
